0509bb9a238353ec9f9a3b481b28a827aaabd8be
KEVIN_FEY <KEVINFEY@gmail.com>
2020-10-01 13:39:12 -0700

    add comments clean code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 
 function App() {
-
+ 
+  // Fetch Data
+  // Use thingproxy to get around cors restrictions
   const getData = () => {
     fetch('https://thingproxy.freeboard.io/fetch/http://fetch-hiring.s3.amazonaws.com/hiring.json'
         )
@@ -12,10 +14,15 @@ function App() {
     .then(data => setList(data));
   }
 
+  // init list to store data from fetch call and setList to update list
   const [list, setList] = useState([])
+  // call getData
   getData()
 
- const listMap = list.sort((a,b)=> a.id - b.id).sort((a,b)=> a.listId - b.listId).map((e) => {
+  // create map 
+  // sort by id and listID
+  // remove null or "" from map
+  const listMap = list.sort((a,b)=> a.id - b.id).sort((a,b)=> a.listId - b.listId).map((e) => {
    if (e.name){
   return (
     <li key={e.id}>
@@ -28,13 +35,8 @@ function App() {
   return ( 
     <>
   <img id="logo" src={logo} alt="fetch-logo"></img>
-    <div className="App">
-  
-      <header className="App-list">
-       
+    <div className="App">    
          <ul>{listMap}</ul>
-
-      </header>
     </div>
     </>
   );
